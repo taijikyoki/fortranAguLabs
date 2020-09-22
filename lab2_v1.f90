ec4e10462a71b0ebc24ae88765d8b7b14cb39e66
Dmitry <36726273+taijikyoki@users.noreply.github.com>
2020-09-22 21:23:18 +0700

    Update lab2_v1.f90

--- lab2_v1.f90
+++ lab2_v1.f90
@@ -1,7 +1,7 @@
 program lab2_v1
     implicit none
     
-    real :: x, y, z, minimal, mid_geometr, half_sum
+    real :: x, y, z, minimal, maximum, middle, mid_geometr, half_sum
     
     read(*,*) x, y, z
     
@@ -11,6 +11,18 @@ program lab2_v1
     if(x <= y .and. x <= z) minimal = x
     if(y <= x .and. y <= z) minimal = y
     if(z <= x .and. z <= y) minimal = z
+    
+    if(x >= y .and. x >= z) maximum = x
+    if(y >= x .and. y >= z) maximum = y
+    if(z >= x .and. z >= y) maximum = z
+    
+    if(x == maximum .and. y == minimum) middle = z
+    if(y == maximum .and. x == minimum) middle = z
+    if(x == maximum .and. z == minimum) middle = y
+    if(z == maximum .and. x == minimum) middle = y
+    if(y == maximum .and. z == minimum) middle = x
+    if(z == maximum .and. y == minimum) middle = x
+    
     
     if(x+y+z<10) then
     minimal = half_sum
